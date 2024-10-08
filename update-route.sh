51967e73e09ec019526e7b3eb5de1eb6abc28acf
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2024-10-08 10:12:32 +0530

    modified:   update-route.sh

--- update-route.sh
+++ update-route.sh
@@ -1,8 +1,23 @@
-#!/bin/zsh
+#!/bin/sh
 
-# Delete route
-sudo route -n delete -net 172.29.248.0/22 192.168.1.64
+# Detect the OS type
+OS=$(uname)
 
-# Add route
-sudo route -n add -net 172.29.248.0/22 192.168.1.64
+# Define the network and gateway
+NETWORK="172.29.248.0/22"
+GATEWAY="192.168.1.64"
 
+if [ "$OS" = "Darwin" ]; then
+    # macOS commands
+    echo "Detected macOS"
+    sudo route -n delete -net $NETWORK $GATEWAY
+    sudo route -n add -net $NETWORK $GATEWAY
+elif [ "$OS" = "Linux" ]; then
+    # Linux commands
+    echo "Detected Linux"
+    sudo ip route delete $NETWORK via $GATEWAY
+    sudo ip route add $NETWORK via $GATEWAY
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
